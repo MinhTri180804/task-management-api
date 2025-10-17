d45ba0e2a0cc0537aece9f94f09c523a907246ab
Minh Tri <nguyenminhtri1808t@gmail.com>
2025-10-17 17:20:32 +0700

    feat(module): import message queue module into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,22 @@
+import { CacheModule } from '@modules/cache/cache.module';
 import { ConfigModule } from '@modules/config/config.module';
+import { MailModule } from '@modules/mail/mail.module';
+import { MessageQueueModule } from '@modules/messageQueue/message-queue.module';
+import { DatabaseModule } from '@modules/mongoose/mongoose.module';
 import { UserModule } from '@modules/user/user.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CacheModule } from '@modules/cache/cache.module';
-import { DatabaseModule } from '@modules/mongoose/mongoose.module';
-import { MailModule } from '@modules/mail/mail.module';
 
 @Module({
-  imports: [ConfigModule, DatabaseModule, UserModule, CacheModule, MailModule],
+  imports: [
+    ConfigModule,
+    DatabaseModule,
+    UserModule,
+    CacheModule,
+    MailModule,
+    MessageQueueModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

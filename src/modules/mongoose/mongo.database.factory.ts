180ab08ec771eed31383d43d31ff80a68c4fa952
Minh Tri <nguyenminhtri1808t@gmail.com>
2025-10-17 13:35:09 +0700

    fix(factory): fix inject connection mongo setup factory

--- src/modules/mongoose/mongo.database.factory.ts
+++ src/modules/mongoose/mongo.database.factory.ts
@@ -1,45 +1,25 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import {
-  InjectConnection,
   MongooseModuleOptions,
   MongooseOptionsFactory,
 } from '@nestjs/mongoose';
-import mongoose, { Connection } from 'mongoose';
+import mongoose from 'mongoose';
 import { AppConfigName, type AppConfig } from 'src/config/app.config';
 import {
   DatabaseConfigName,
   type DatabaseConfig,
 } from 'src/config/database.config';
-import { DatabaseModule } from './mongoose.module';
 
 @Injectable()
 export class MongoDatabaseSetupFactory implements MongooseOptionsFactory {
   private readonly _DEFAULT_SELECTION_TIMEOUT = 60; // Second value
   private readonly _DEFAULT_SOCKET_TIMEOUT = 120; // Second value
   private readonly _DEFAULT_AUTO_INDEX = true;
-  private readonly _logger = new Logger(DatabaseModule.name);
 
-  constructor(
-    private readonly _configService: ConfigService,
-    @InjectConnection() private readonly _connection: Connection,
-  ) {}
+  constructor(private readonly _configService: ConfigService) {}
 
   // TODO: Fix show log related connection mongodb later in here
-  private _emitEventListenConnection() {
-    this._connection.on('connected', () =>
-      this._logger.log('✅ MongoDB connected successfully'),
-    );
-    this._connection.on('error', (err) =>
-      this._logger.error('❌ MongoDB connection error', err),
-    );
-    this._connection.on('disconnected', () =>
-      this._logger.warn('⚠️ MongoDB disconnected'),
-    );
-    this._connection.on('reconnected', () =>
-      this._logger.log('🔄 MongoDB reconnected'),
-    );
-  }
 
   createMongooseOptions():
     | Promise<MongooseModuleOptions>

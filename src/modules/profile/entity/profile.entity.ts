d168952caed91d41a61e935c3da34cd5f187bf3b
Minh Tri <nguyenminhtri1808t@gmail.com>
2025-10-19 16:45:33 +0700

    feat(entity): implement user_id field in schema

--- src/modules/profile/entity/profile.entity.ts
+++ src/modules/profile/entity/profile.entity.ts
@@ -1,6 +1,7 @@
+import { User } from '@modules/user/entity/user.entity';
 import { ModelDefinition, Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { BaseEntity } from '@shared/entities/base/base.entity';
-import { HydratedDocument } from 'mongoose';
+import { HydratedDocument, Types } from 'mongoose';
 
 const AVATAR_DEFAULT_URL =
   'https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small_2x/default-avatar-icon-of-social-media-user-vector.jpg';
@@ -12,6 +13,9 @@ export type ProfileDocument = HydratedDocument<Profile>;
 
 @Schema({ collection: COLLECTION_NAME, versionKey: false, timestamps: true })
 export class Profile extends BaseEntity {
+  @Prop({ type: Types.ObjectId, required: true, ref: User.name })
+  user_id: User;
+
   @Prop({ type: String, required: true })
   first_name: string;
 

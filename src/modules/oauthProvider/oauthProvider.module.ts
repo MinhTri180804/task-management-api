f2ba1420da49fdf9008a9e7b31a06fd8f2fbc73c
Minh Tri <nguyenminhtri1808t@gmail.com>
2025-10-19 15:32:13 +0700

    refactor(module): refactor oauth provider module

--- src/modules/oauthProvider/oauthProvider.module.ts
+++ src/modules/oauthProvider/oauthProvider.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { OAuthProviderService } from './oauthProvider.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { OAuthProviderModel } from './entity/oauth-provider.entity';
 
 @Module({
-  imports: [],
-  providers: [],
-  exports: [],
+  imports: [MongooseModule.forFeature([OAuthProviderModel])],
+  providers: [OAuthProviderService],
+  exports: [MongooseModule, OAuthProviderService],
 })
 export class OAuthProviderModule {}

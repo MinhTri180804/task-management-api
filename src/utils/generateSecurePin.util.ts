3a46e147176e8f8b5ffea5f501847d50e70cc036
Minh Tri <nguyenminhtri1808t@gmail.com>
2025-10-20 00:09:06 +0700

    refactor(util): change params type for generateSecurePin util function

--- src/utils/generateSecurePin.util.ts
+++ src/utils/generateSecurePin.util.ts
@@ -1,11 +1,9 @@
-type Params = { length?: number };
-
-export const generateSecurePin = ({ length = 6 }: Params): string => {
-  const value: number[] = [];
-
+export const generateSecurePin = (length = 6): string => {
   if (length <= 0) {
     return '';
   }
+
+  const value: number[] = [];
 
   for (let i = 0; i < length; i++) {
     const digit = Math.floor(Math.random() * 10);

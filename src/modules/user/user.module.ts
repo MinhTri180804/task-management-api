13f64fa31cae0abeeccd87a585eae9c3246fc4da
Minh Tri <nguyenminhtri1808t@gmail.com>
2025-10-19 15:32:46 +0700

    refactor(module): refactor user module

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModel } from './entity/user.entity';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forFeature([UserModel])],
   providers: [UserService],
   controllers: [UserController],
-  exports: [],
+  exports: [MongooseModule, UserService],
 })
 export class UserModule {}

690dddf4a48002cb0941fad3d3fb0802836f012f
Minh Tri <nguyenminhtri1808t@gmail.com>
2025-10-20 00:04:28 +0700

    feat(repository): implement user repository layer for user module

---
+++ src/modules/user/repository/user.repository.ts
@@ -0,0 +1,27 @@
+import { BaseRepositoryAbstract } from '@shared/repositories/base/base.abstract.repository';
+import { User } from '../entity/user.entity';
+import {
+  FindUserByEmailPrams,
+  IUserRepository,
+} from '../interface/user-repository.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+export class UserRepository
+  extends BaseRepositoryAbstract<User>
+  implements IUserRepository
+{
+  constructor(
+    @InjectModel(User.name) private readonly _userModel: Model<User>,
+  ) {
+    super(_userModel);
+  }
+
+  async findUserByEmail({ email }: FindUserByEmailPrams) {
+    return await this._userModel
+      .findOne({
+        email,
+      })
+      .exec();
+  }
+}

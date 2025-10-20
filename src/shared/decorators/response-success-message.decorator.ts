b5513c1a3c35fb3c8d99990c98af25e3c8cd1f6f
Minh Tri <nguyenminhtri1808t@gmail.com>
2025-10-20 16:27:37 +0700

    refactor(decorator): refactor responseSuccessMessage decorator

--- src/shared/decorators/response-success-message.decorator.ts
+++ src/shared/decorators/response-success-message.decorator.ts
@@ -1,6 +1,6 @@
 import { SetMetadata } from '@nestjs/common';
 
-export const RESPONSE_SUCCESS_MESSAGE_METADATA = 'responseSuccessMessage';
+export const RESPONSE_SUCCESS_MESSAGE_METADATA = 'response_success_message';
 
 export const ResponseSuccessMessage = (message: string) =>
   SetMetadata(RESPONSE_SUCCESS_MESSAGE_METADATA, message);

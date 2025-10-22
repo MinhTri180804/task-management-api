d1484d4dcf035b7bfe4a3c24aea67d282e6b81c3
Minh Tri <nguyenminhtri1808t@gmail.com>
2025-10-22 13:29:43 +0700

    feat(cache-service): implement method delete-verify-email-register

--- src/core/cache/cache.service.ts
+++ src/core/cache/cache.service.ts
@@ -3,6 +3,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import { CACHE_VERIFY_EMAIL_REGISTER_KEY } from './cache.constant';
 import { VerifyEmailRegisterPayload } from './cache.type';
 
+// ====== Types Params ======
 type SaveVerifyEmailRegisterParams = {
   email: string;
   otp: string;
@@ -10,22 +11,31 @@ type SaveVerifyEmailRegisterParams = {
 
 type GetVerifyEmailRegisterParams = { email: string };
 
+type DeleteVerifyEmailRegisterParams = { email: string };
+
+// ====== Types Return ======
 type SaveVerifyEmailRegisterReturn = {
   expiredAt: Date;
 };
 
 type GetVerifyEmailRegisterReturn = VerifyEmailRegisterPayload;
 
+type DeleteVerifyEmailRegisterReturn = Promise<boolean>;
+
 @Injectable()
 export class CacheService {
   constructor(@Inject(CACHE_MANAGER) private readonly _cacheManager: Cache) {}
+
+  private _getKeyVerifyEmailRegister({ email }: { email: string }) {
+    return `${CACHE_VERIFY_EMAIL_REGISTER_KEY}:${email}`;
+  }
 
   async getVerifyEmailRegister({
     email,
   }: GetVerifyEmailRegisterParams): Promise<
     GetVerifyEmailRegisterReturn | undefined
   > {
-    const KEY = `${CACHE_VERIFY_EMAIL_REGISTER_KEY}:${email}`;
+    const KEY = this._getKeyVerifyEmailRegister({ email });
     return await this._cacheManager.get(KEY);
   }
 
@@ -34,7 +44,7 @@ export class CacheService {
     otp,
   }: SaveVerifyEmailRegisterParams): Promise<SaveVerifyEmailRegisterReturn> {
     const TTL_MINUTE = 3;
-    const KEY = `${CACHE_VERIFY_EMAIL_REGISTER_KEY}:${email}`;
+    const KEY = this._getKeyVerifyEmailRegister({ email });
     const TTL = TTL_MINUTE * 60;
 
     const createdAt = Date.now();
@@ -49,5 +59,12 @@ export class CacheService {
     );
 
     return { expiredAt };
+  }
+
+  async deleteVerifyEmailRegister({
+    email,
+  }: DeleteVerifyEmailRegisterParams): DeleteVerifyEmailRegisterReturn {
+    const KEY = this._getKeyVerifyEmailRegister({ email });
+    return await this._cacheManager.del(KEY);
   }
 }

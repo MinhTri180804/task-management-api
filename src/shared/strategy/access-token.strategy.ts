210b5aff010702e759ccd002e6bcce1dd0d09b88
Minh Tri <nguyenminhtri1808t@gmail.com>
2026-01-11 07:26:22 +0700

    feat(guard-strategy): add guard and strategy for jwt access token

---
+++ src/shared/strategy/access-token.strategy.ts
@@ -0,0 +1,26 @@
+import {
+  SecretKeyConfig,
+  SecretKeyConfigName,
+} from '@config/secret-key.config';
+import { JWTAccessTokenPayload } from '@core/jwt/accessToken/types/payload.type';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class AccessTokenStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly _configService: ConfigService) {
+    const { accessToken } =
+      _configService.getOrThrow<SecretKeyConfig>(SecretKeyConfigName);
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: accessToken,
+    });
+  }
+
+  validate(payload: JWTAccessTokenPayload): JWTAccessTokenPayload {
+    return payload;
+  }
+}

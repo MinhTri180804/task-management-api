c65502ae8a8a11729c5669c7482c1c62f6d06b60
Minh Tri <nguyenminhtri1808t@gmail.com>
2026-02-15 08:30:57 +0700

    feat(main): add MongoServerException into global exception

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
+import { MongoServerExceptionFilter } from '@shared/filters/mongo/mongo-server-error.filter';
 import { NestFactory, Reflector } from '@nestjs/core';
-import { StrictValidationPipe } from '@shared/pipes/strict-validation.pipe';
-import { AppModule } from './app.module';
 import { AllExceptionFilter } from '@shared/filters/all-exception.filter';
 import { ResponseTransformInterceptor } from '@shared/interceptor/response-transform.interceptor';
+import { StrictValidationPipe } from '@shared/pipes/strict-validation.pipe';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new StrictValidationPipe());
-  app.useGlobalFilters(new AllExceptionFilter());
+  app.useGlobalFilters(
+    new AllExceptionFilter(),
+    new MongoServerExceptionFilter(),
+  );
   app.useGlobalInterceptors(new ResponseTransformInterceptor(new Reflector()));
   await app.listen(process.env.APP_PORT ?? 3000);
   console.log(

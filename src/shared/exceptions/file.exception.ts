483f118acb3d823199d9a317900b535072ed7c1f
Minh Tri <nguyenminhtri1808t@gmail.com>
2026-02-26 11:47:04 +0700

    refactor(exception): replace type saft params of file exception

--- src/shared/exceptions/file.exception.ts
+++ src/shared/exceptions/file.exception.ts
@@ -1,10 +1,21 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
+import {
+  ERROR_CODE,
+  ErrorCodeValues,
+} from '@shared/constants/error-code.constant';
 
-const DEFAULT_ERROR_CODE = 'FILE_ERROR';
+const DEFAULT_ERROR_CODE = ERROR_CODE.FILE_ERROR;
 const DEFAULT_STATUS_CODE = HttpStatus.BAD_REQUEST;
+const DEFAULT_MESSAGE = 'File error';
 
 export class FileException extends HttpException {
-  constructor({ message, errorCode = DEFAULT_ERROR_CODE }) {
+  constructor({
+    message = DEFAULT_MESSAGE,
+    errorCode = DEFAULT_ERROR_CODE,
+  }: {
+    message: string;
+    errorCode: ErrorCodeValues;
+  }) {
     super(
       {
         message,

a11e102d2e1dfd0b07057e399d40fa3e993b9d41
Minh Tri <nguyenminhtri1808t@gmail.com>
2026-02-26 11:43:55 +0700

    feat(validator): add protected method getWidthHeightByShard into this abstract class

--- src/shared/validator/file/image/file-validator.abstract.validator.ts
+++ src/shared/validator/file/image/file-validator.abstract.validator.ts
@@ -1,12 +1,17 @@
 import { FileValidator } from '@nestjs/common';
 import { ErrorCodeValues } from '@shared/constants/error-code.constant';
 import { mergeErrorCodeMessage } from '@shared/utils/merge-error-code-message.util';
+import sharp from 'sharp';
 
 export abstract class FileValidatorAbstract<T> extends FileValidator<
   T & { message?: string; hasMetadata: boolean }
 > {
   abstract readonly ERROR_CODE: ErrorCodeValues;
   abstract readonly DEFAULT_MESSAGE: string;
+
+  protected getWidthHeightByShard(file: Express.Multer.File) {
+    return sharp(file.buffer).metadata();
+  }
 
   buildErrorMessage(): string {
     return mergeErrorCodeMessage({
